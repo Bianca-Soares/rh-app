1bc5e6c791f3da37a2f20e071e5b6ed752068a5f
Bianca Soares <biancasoaresdasilva2016@gmail.com>
2024-04-30 17:39:40 -0300

    add componentes NavBar, Usuario, ListaUsuario

---
+++ src/components/ListaUsuario.vue
@@ -0,0 +1,62 @@
+<template>
+  <h1>Lista de usuários</h1>
+  <div>
+    <div class="lista">
+      <div v-for="item in listaPessoas" :key="item.id">
+        <Usuario :usuario="item"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Usuario from "@/components/Usuario.vue";
+export default {
+  components: {
+    Usuario,
+  },
+  setup() {
+    const listaPessoas = [
+      {
+        id: 7,
+        email: "michael.lawson@reqres.in",
+        first_name: "Michael",
+        last_name: "Lawson",
+        avatar: "https://reqres.in/img/faces/7-image.jpg",
+      },
+      {
+        id: 8,
+        email: "lindsay.ferguson@reqres.in",
+        first_name: "Lindsay",
+        last_name: "Ferguson",
+        avatar: "https://reqres.in/img/faces/8-image.jpg",
+      },
+      {
+        id: 9,
+        email: "tobias.funke@reqres.in",
+        first_name: "Tobias",
+        last_name: "Funke",
+        avatar: "https://reqres.in/img/faces/9-image.jpg",
+      }
+    ]
+    return {
+      listaPessoas
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+  text-align: center;
+}
+
+.lista {
+  display: grid;
+  margin: 0 20px;
+  grid-template-columns: repeat(4, minmax(200px, 400px));
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+</style>
---
+++ src/components/Usuario.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="card">
+    <div class="imagem-container">
+      <img src="@/assets/logo.png" alt="imagem avatar" />
+    </div>
+    <span> Nome </span>
+    <a class="favorito" >
+      <img v-if="favorito" src="@/assets/star-svgrepo.svg" alt="favorito icone" />
+      <img v-else src="@/assets/star-outline.svg" alt="favorito icone" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["usuario"]
+}
+</script>
+
+<style>
+.card {
+  padding: 10px;
+  background: #ffedec;
+  border-radius: 5px;
+  position: relative;
+}
+
+.imagem-container {
+  width: 100px;
+  height: 100px;
+  margin: 0 auto;
+  padding-top: 10px;
+}
+
+.imagem-container img {
+  border-radius: 50%;
+}
+
+.card span {
+  margin-top: 10px;
+  display: block;
+  text-align: center;
+}
+
+.favorito {
+  position: absolute;
+  top: 10px;
+  right: 5px;
+  cursor: pointer;
+}
+
+.favorito img {
+  width: 30px;
+  margin: 0 auto;
+}
+</style>
