987c4caa359844d5002f2b8063677ded9c59d9dc
Bianca Soares <biancasoaresdasilva2016@gmail.com>
2024-04-30 17:37:39 -0300

    add view home, favoritos

---
+++ src/views/FavoritosView.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Lista de favoritos</h1>
+  <div class="lista">
+    <div v-for="item in listaFavoritos" :key="item.id">
+      <div class="card">
+        <div class="imagem-container">
+          <img :src="item.avatar" alt="imagem avatar" />
+        </div>
+        <span>{{ item.first_name + " " + item.last_name }}</span>
+        <a class="favorito" @click="removeFavorito(item.id)">
+          <span>X</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  components: {},
+  setup() {
+    const listaFavoritos = ref([]);
+  },
+}
+</script>
+
+<style scoped>
+.favorito span {
+  font-size: 25px;
+  color: #f04f47;
+  text-align: center;
+}
+</style>
